f6900ad07d429ea63aedbc5bbae6cc8ff85d5758
Mikita Buinavets <mikita.buinavets@arua.co>
2025-06-30 05:09:05 +0300

    Fix best engineering practices

--- src/main/java/assignments/LocalProcessor.java
+++ src/main/java/assignments/LocalProcessor.java
@@ -1,35 +1,37 @@
 package assignments;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.List;
-import java.util.Objects;
-import java.util.Scanner;
-
 import assignments.annotations.FullNameProcessorGeneratorAnnotation;
 import assignments.annotations.ListIteratorAnnotation;
 import assignments.annotations.ReadFullProcessorNameAnnotation;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+
 @Getter
 @Setter
 public class LocalProcessor {
-    private String processorName = "";
+    private String processorName;
     private Long period = 10_000_000_000_000L;
-    private String processorVersion = "";
+    private String processorVersion;
     private Integer valueofCheap;
-    private Scanner informationscanner;
+    private Scanner informationScanner;
     private StringBuilder builder;
-    private List<String> stringList;
+    private List<String> stringArrayList = new LinkedList<>();
 
     public LocalProcessor(String processorName, Long period, String processorVersion, Integer valueOfCheap,
-                          List<String> stringList) {
+                          Scanner informationScanner, List<String> stringArrayList) {
         this.processorName = processorName;
         this.period = period;
         this.processorVersion = processorVersion;
         this.valueofCheap = valueOfCheap;
-        this.stringList = stringList;
+        this.informationScanner = informationScanner;
+        this.stringArrayList = stringArrayList;
     }
 
     public LocalProcessor() {
@@ -54,14 +56,18 @@ public class LocalProcessor {
     }
 
     @ReadFullProcessorNameAnnotation
-    public void readfullprocessorname(File file) throws FileNotFoundException {
+    public void readfullprocessorname(File file) {
         try (Scanner scanner = new Scanner(file)) {
-            this.informationscanner = scanner;
+            informationScanner = scanner;
             builder = new StringBuilder(processorVersion);
             while (scanner.hasNext()) {
                 builder.append(scanner.nextLine());
             }
             processorVersion = builder.toString();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            informationScanner.close();
         }
     }
 }
